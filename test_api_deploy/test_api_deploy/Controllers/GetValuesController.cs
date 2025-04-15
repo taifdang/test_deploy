01995c7beb23878012b5bfb8047b32d9f8bb812c
LAPTOP-J20BGGNG\taida <taidangduc1503@gmail.com>
2025-04-15 22:39:55 +0700

    add new project (#1)

---
+++ test_api_deploy/test_api_deploy/Controllers/GetValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using test_api_deploy.Models;
+
+namespace test_api_deploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetValuesController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        string connectionString;
+        public GetValuesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            connectionString = configuration.GetConnectionString("Database") ?? string.Empty;
+        }
+        [HttpPost]
+        public async Task<IActionResult> get()
+        {
+            var data = new List<User>();
+            var query = "select*from users";
+            using var connection = new SqlConnection(connectionString);
+            data = connection.Query<User>(query).ToList();
+
+            return Ok(data);
+        }
+    }
+}
---
+++ test_api_deploy/test_api_deploy/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace test_api_deploy.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+       
+    }
+}
